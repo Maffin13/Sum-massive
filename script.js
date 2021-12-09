76297000257a710bb2fb3187fd4272bf351dab62
Maffin <maftuna.nova@gmail.com>
2021-12-09 11:59:33 +0500

    sum

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-function sum(x){
+function sum(num){
    let a = 0;
-   for(let i = 0; i < x.length; i++)
-      {a = a + x[i]}
-      console.log(a);
+   for(let i = 0; i < num.length; i++)
+      {a = a + num[i]}
       return a;
    }
    
